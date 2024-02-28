af9760f3c50fe5c1ecf1f154e92165e0e4eb8c64
andressamachado <andressa.pamachado@gmail.com>
2024-02-27 19:37:16 -0500

    Simplify code solution; Add comments

--- lib/version_manager.rb
+++ lib/version_manager.rb
@@ -1,35 +1,42 @@
 # frozen_string_literal: true
 
-# == Requirement ==
-# No number but the first may be greater than 9
-# if any are, you must set them to 0 and increment the next number in sequence.
+##
+# Class to represent a version numbering calculator
+# Enables to create an object to perform calculations on a String holding a sequence of numbers separated
+# by dots, representing a version number.
+# Example:
+#   version_manager = VersionManager.new
+#   version_manager.next_version('1.2.3') # => '1.2.4'
+#   version_manager.next_version('9.9.9') # => '10.0.0'
 class VersionManager
-  # Function to get the next version to be installed
-  def nextVersion(current_version)
-    # reverse array to start process from the last segment
-    version_segments = current_version.split('.').map(&:to_i).reverse
-    process(version_segments)
-  end
+  def initialize; end
 
-  private
+  ##
+  # Method to process the next version number based on the current version number given by the user
+  # @param [String] current_version - The current version number
+  # @return [String] updated_version - The next version number
+  def next_version(current_version)
+     # Transform from string to array of integers
+     partials = current_version.split('.').map(&:to_i)
 
-  # Function to process the version segments
-  def process(version_segments)
-    version_segments.each_with_index do |segment, index|
-      if segment == 9
-        major_segment = version_segments.length - 1
-        # -> major segment increase freely
-        # if not the major segment:
-        # we set current segment to 0 and keep looking for a number less than 9 to increment and terminate the loop
-        index == major_segment ? version_segments[index] += 1 : version_segments[index] = 0
-      else
-        # when we find a number less than 9, we increment it and break the loop
-        version_segments[index] += 1
-        break
-      end
-    end
+     # Going back from end to start of array
+     (partials.length - 1).downto(0) do |index|
+       # increment if its 9 by setting to 0, and continue to next number
+       if partials[index] == 9
+         # if it is the major element of the version, we increase regardeless the current number
+         index == 0 ? partials[index] += 1 : partials[index] = 0
+       else
+         # if its less than 9, we increment and stop the loop, since we dont carry over any amount in this case
+         partials[index] +=  1
+         break
+       end
+     end
 
-    # reverse back the version segments
-    version_segments.reverse.join('.')
+     # Builds the string version again and return
+     updated_version = partials.join('.')
   end
 end
+
+version_manager = VersionManager.new
+puts version_manager.next_version('9.9.9') # => '10.0.0'
+puts version_manager.next_version('1.2.3') # => '1.2.4'
